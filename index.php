a1625c7cceaddac7f81b9f2c1abf289b51689010
Amita <amiage.owl@outlook.jp>
2022-12-10 20:29:54 +0900

    first

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <META http-equiv="Refresh" content="4;URL=home.php">
+    <title>Portfolio</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+    <div id="load">
+        <p class="portfolio">Portfolio</p>
+            <img src="/image/1669510983010.jpg"  class="kotechar" alt="">
+        <p class="michu">now loading...</p>
+        <div class="progBar">
+        <p class="bar"></p>
+</div>
+    </div>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="  crossorigin="anonymous"></script>
+<script type="module" src="/js/script.js"></script>
+</body>
+</html>
